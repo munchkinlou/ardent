0f2202908a873a668fde8afa14f01ba54f9b7927
munchkinlou <arlplanells@gmail.com>
2025-08-29 20:09:49 -0600

    Create okiku_profile.js

---
+++ shoprequest/cloi/okiku/okiku_profile.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.blocky span').forEach(span => {
+        if (span.textContent.toLowerCase().includes('personality') || 
+            span.textContent.toLowerCase().includes('appearance') || 
+            span.textContent.toLowerCase().includes('misc')) {
+            span.style.display = 'none';
+        }
+    });
+
+  document.querySelectorAll('.blocky').forEach(blocky => {
+      if (blocky.textContent.toLowerCase().includes('hideme')) {
+          blocky.style.border = 'none';
+          blocky.style.background = 'transparent';
+          blocky.style.boxShadow = 'none';
+          blocky.style.backdropFilter = 'none';
+          blocky.style.padding = '0';
+          blocky.classList.add('nobefore');
+          blocky.style.margin = '20px 10px 0';
+      }
+  });
+});
